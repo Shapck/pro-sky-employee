bd4be2baa8cfc469598590695bd11441185351aa
Дима <dimonsoldatov@gmsil.com>
2022-05-07 15:42:40 +0300

    Initial commit

---
+++ src/main/java/pro/sky/employee/proskyemployee/ProSkyEmployeeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employee.proskyemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSkyEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProSkyEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/employee/proskyemployee/ProSkyEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employee.proskyemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProSkyEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
